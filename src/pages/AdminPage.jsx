aefeb005cef6e5f9884bd458aa809f9e25e71f3e
AlienMinus <dasmanasranjan2005@gmail.com>
2025-11-25 21:59:27 +0530

    modified:   src/components/Contact.jsx 	modified:   src/pages/AdminPage.jsx

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -15,7 +15,7 @@ function AdminPage() {
     if (!authed && !withLoading) return;
     if (withLoading) setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/admin/messages", {
+      const res = await fetch("https://manas.up.railway.app/api/admin/messages", {
         headers: {
           "x-admin-password": password,
         },
@@ -42,7 +42,7 @@ function AdminPage() {
     setStatus(null);
 
     try {
-      const res = await fetch("http://localhost:5000/api/admin/messages", {
+      const res = await fetch("https://manas.up.railway.app/api/admin/messages", {
         headers: {
           "x-admin-password": password,
         },
@@ -86,7 +86,7 @@ function AdminPage() {
     try {
       setLoading(true);
       const res = await fetch(
-        `http://localhost:5000/api/admin/messages/${encodeURIComponent(
+        `https://manas.up.railway.app/api/admin/messages/${encodeURIComponent(
           timestamp
         )}`,
         {
